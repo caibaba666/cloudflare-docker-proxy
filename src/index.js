597f6d9bdf92fa59be637996b8dc5ef31d1cd69c
caibaba666 <105051341+caibaba666@users.noreply.github.com>
2024-12-14 15:30:33 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.closeai.me": dockerHub,
-  "quay.closeai.me": "https://quay.io",
-  "gcr.closeai.me": "https://gcr.io",
-  "k8s-gcr.closeai.me": "https://k8s.gcr.io",
-  "k8s.closeai.me": "https://registry.k8s.io",
-  "ghcr.closeai.me": "https://ghcr.io",
-  "cloudsmith.closeai.me": "https://docker.cloudsmith.io",
+  "docker.caibaba.eu.org": dockerHub,
+  "quay.caibaba.eu.org": "https://quay.io",
+  "gcr.caibaba.eu.org": "https://gcr.io",
+  "k8s-gcr.caibaba.eu.org": "https://k8s.gcr.io",
+  "k8s.caibaba.eu.org": "https://registry.k8s.io",
+  "ghcr.caibaba.eu.org": "https://ghcr.io",
+  "cloudsmith.caibaba.eu.org": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.closeai.me": dockerHub,
+  "docker-staging.caibaba.eu.org": dockerHub,
 };
 
 function routeByHosts(host) {
